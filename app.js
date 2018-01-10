e6fa66c932e192d45857200c2de7da542f94ab80
nadar123 <nadar.rosano@gmail.com>
2018-01-10 12:07:16 +0200

    new

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+app.listen(8080, function () {
+    console.log('server is ON' + process.env.PORT);
+
 });
